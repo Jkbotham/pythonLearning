5237738e842d832f24d61300f066105b3329238a
jkbotham <jamesbotham@gmail.com>
2021-11-05 17:03:13 -0500

    Added validation to input values

--- numberGuessing.py
+++ numberGuessing.py
@@ -3,7 +3,19 @@ import random
 print("Welcome to the Number Guessing Game! Good Luck")
 print()
 
-x = int(input("How big of range should we use? "))
+
+x = input("How big of range should we use? ")
+
+if x.isdigit():
+    x = int(x)
+    if x <= 0:
+        print("Please enter number greater than 0 next time.")
+        quit()
+else:
+    print("Please enter a number next time")
+    quit()
+
+
 
 number = random.randint(1,x)
 
