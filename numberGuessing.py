c08d97733deb651ae10531889e9c61935d87aff9
jkbotham <jamesbotham@gmail.com>
2021-11-05 16:40:40 -0500

    changed randrange to randint

--- numberGuessing.py
+++ numberGuessing.py
@@ -5,24 +5,22 @@ print()
 
 x = int(input("How big of range should we use? "))
 
-number = random.randrange(1,(x+1))
+number = random.randint(1,x)
 
 def question(): 
     guess = input("Guess a number between 1 and " + str(x) + "! ")
 
     if int(guess) > x:
         print("That guess is not within the range. Guess a number between 1 and " + str(x) + "! ")
+        print()
         question()
 
     if int(guess) != number: 
         print("That is incorrect")
+        print()
         question()
     else:
         print("Thats correct! The number is " + str(number) + "!")
         quit()
 
-
 question()
-    
-
-
